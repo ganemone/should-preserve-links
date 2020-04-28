25dbed728210049fdca13bdf5b24bda06c29dd14
Giancarlo Anemone <ganemone@uber.com>
2020-04-28 13:17:52 -0400

    Add check for environment variable

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 module.exports = function shouldPreserveLinks() {
+  if (Boolean(process.env.NODE_PRESERVE_SYMLINKS)) {
+    return true;
+  }
   const filename = require("./b");
   if (filename.endsWith("b.js")) {
     return true;
