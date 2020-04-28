e8c8b9a163a926f4bceae5ab9c493efea89e69d7
Giancarlo Anemone <ganemone@uber.com>
2020-04-28 14:37:15 -0400

    Use different approach

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
+const os = require('os');
+const fs = require('fs');
+const path = require('path');
+
+const pathToA = path.join(__dirname, 'a.js');
+const pathToB = path.join(__dirname, 'b.js');
+
 module.exports = function shouldPreserveLinks() {
   if (Boolean(process.env.NODE_PRESERVE_SYMLINKS)) {
     return true;
+  }
+  if (!fs.existsSync(pathToB)) {
+    fs.symlinkSync(pathToA, pathToB);
   }
   const filename = require("./b");
   if (filename.endsWith("b.js")) {
--- postinstall.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-
-try {
-  fs.unlinkSync(path.join(__dirname), 'b.js');
-  fs.symlinkSync(path.join(__dirname, 'a.js'), path.join(__dirname, 'b.js'));
-} catch(e) {}
-
