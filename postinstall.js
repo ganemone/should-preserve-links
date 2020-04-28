08cb57153a598d9f01f5baf4861bb49adcd1d1ef
Giancarlo Anemone <ganemone@uber.com>
2020-04-28 12:22:31 -0400

    Use postinstall

---
+++ postinstall.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const fs = require('fs');
+
+try {
+  fs.unlinkSync(path.join(__dirname), 'b.js');
+} catch(e) {}
+
+fs.symlinkSync(path.join(__dirname, 'a.js'), path.join(__dirname, 'b.js'));
